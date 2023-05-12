3e5131c570cfe4cac102aeecf9226a09354ca9a0
Carolin Meier <carolin.meier@cbc.de>
2023-05-12 14:26:52 +0200

    feat: add and use babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './src/client/index.js',
-}
+  entry: "./src/client/index.js",
+  //   ouput: {},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
